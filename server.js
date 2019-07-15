0dc530c8362b59db23e5e274f0009c060986c463
Dameon Brown <axel12gt@live.com>
2019-07-15 13:45:31 -0400

    set up server routes to about us

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ var PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(express.static("public/imgs"))
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}))
 app.set("view engine", "handlebars")
 
-app.get("/", function(req, res){
-    res.render("index", {})
-})
+require("./routes/html-routes.js")(app)
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT)
