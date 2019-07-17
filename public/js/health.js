fcb01e707e276bbb28da8a7ed0ce60bb45cf7484
Dameon Brown <axel12gt@live.com>
2019-07-17 13:17:30 -0400

    double checked pages fixed signup routing

--- public/js/health.js
+++ public/js/health.js
@@ -2,14 +2,6 @@ $(document).ready(function(){
 
     var number = document.getElementById('number');
 
-// Listen for input event on numInput.
-number.onkeydown = function(e) {
-    if(!((e.keyCode > 95 && e.keyCode < 106)
-      || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
-        return false;
-    }
-}
 
     // var $newFirstName = $("input.new-FirstName")
     // var $newLastName = $("input.new-LastName")
